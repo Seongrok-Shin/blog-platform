ee167609663278c030fbb80feb48fa935212dc17
Seongrok Shin <ssr7324@gmail.com>
2025-02-19 20:10:26 +1300

    fix(error): earch params
    Types of property 'searchParams' are incompatible.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -20,7 +20,7 @@ async function getSearchResults(
 export default async function SearchPage({
   searchParams,
 }: {
-  searchParams: { query: string; filter?: string };
+  searchParams: Promise<{ query: string; filter?: string }>;
 }) {
   // Ensure searchParams is awaited
   const { query, filter = "all" } = await searchParams;
